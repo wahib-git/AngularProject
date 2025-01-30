baa117b69745cf6a867c3c7a9f7844a8e6f24ede
Wahib <wahib bachoua>
2025-01-30 21:29:48 +0100

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MyFirstCompComponent } from './my-first-comp/my-first-comp.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, MyFirstCompComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'angular-crach-course';
---
+++ src/app/my-first-comp/my-first-comp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-first-comp',
+  imports: [],
+  templateUrl: './my-first-comp.component.html',
+  styleUrl: './my-first-comp.component.scss'
+})
+export class MyFirstCompComponent {
+
+}
